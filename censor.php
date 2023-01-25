f02ca63eb08050dada26245b5752a64fe8e7817a
Vincenzo Ruggiero <vincenzoru@live.it>
2023-01-25 17:09:09 +0100

    Refactoring

--- censor.php
+++ censor.php
@@ -1,24 +1,36 @@
-
-    <?php 
+<?php 
    $sentence =  $_POST['sentence'];
    $censored =  $_POST['word'];
 
    $changed = str_ireplace($censored, '***', $sentence)
-    ?>
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 
 <h2>
-<?php echo $sentence ?>
+    <?php echo $sentence ?>
 </h2>
 
 <p>
-Il paragrafo scritto è lungo <strong><?php echo strlen($sentence);?></strong> caratteri.
+    Il paragrafo scritto è lungo <strong><?php echo strlen($sentence);?></strong> caratteri.
 </p>
 
 <h2>
-<?php echo $changed?>
-</h2>
+    <?php echo $changed?>
+    </h2>
 <p>
-Il paragrafo scritto è lungo <strong><?php echo strlen($changed)?></strong> caratteri.
+    Il paragrafo scritto è lungo <strong><?php echo strlen($changed)?></strong> caratteri.
 </p>
+
+</body>
+</html>
 
 
